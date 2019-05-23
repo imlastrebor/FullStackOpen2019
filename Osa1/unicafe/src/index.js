89f609558da5e1cba9d3b61bdd2493c7f656b279
Robert Salmi <Robert.salmi@hotmail.com>
2019-05-23 10:48:18 +0300

    Kohdat 1.08, 1.09 ja 1.10

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -15,7 +15,7 @@ const Button = ({handleClick, text}) =>(
 
 )
 
-const Statistics = ({text, value}) => {
+const Statistic = ({text, value}) => {
     return(
         <div>
             <p>{text} {value}</p>
@@ -23,66 +23,84 @@ const Statistics = ({text, value}) => {
     )
 }
 
+const Statistics = (props) => {
 
+    if (props.valueTotal < 1) {
+        return(
+            <p>Ei yhtään palautetta</p>
+        )
+    }
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [avg, setAvg] = useState(0)
-  const [positive, setPositive] = useState(0)
+    return(
+        <div>
+            <Header headerText="statistiikka"/>
+            <Statistic text="hyvä" value={props.valueGood}/>
+            <Statistic text="neutraali" value={props.valueNeutral}/>
+            <Statistic text="huono" value={props.valueBad}/>
+            <Statistic text="yhteensä" value={props.valueTotal}/>
+            <Statistic text="keskiarvo" value={props.valueAvg}/>
+            <Statistic text="positiivista" value={props.valuePositive}/>
+        </div>
+    )
 
-  const handleClickGood = () => {
-    setGood(good + 1)
-    setTotal(good + neutral + bad + 1)
-    setAvg(((good + 1) + (-1 * bad)) / (total + 1))
-    setPositive(((good + 1) / (total + 1)) * 100 + '%')  }
-
-  const handleClickNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(good + neutral + bad + 1)
-    setPositive(((good) / (total + 1)) * 100 + '%')  
-    setAvg((good + (-1 * bad)) / (total + 1))
 }
 
-  const handleClickBad = () => {
-    setBad(bad + 1)
-    setTotal(good + neutral + bad + 1)
-    setPositive(((good) / (total + 1)) * 100 + '%')
-    setAvg((good + (-1 * (bad + 1 ))) / (total + 1))
-  }
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [avg, setAvg] = useState(0)
+    const [positive, setPositive] = useState(0)
 
-  return (
-    <div>
-    <Header headerText="Anna palautetta"/>
-    <Button 
-        handleClick={handleClickGood}
-        text="Good"
-    />
-        <Button 
-        handleClick={handleClickNeutral}
-        text="Neutral"
-    />
-        <Button 
-        handleClick={handleClickBad}
-        text="Bad"
-    />
-    <Header headerText="statistiikka" />
-    <Statistics text="hyvä" value={good}/>
-    <Statistics text="neutraali" value={neutral}/>
-    <Statistics text="huono" value={bad}/>
-    <Statistics text="yhteensä" value={total}/>
-    <Statistics text="keskiarvo" value={avg}/>
-    <Statistics text="positiivista" value={positive}/>
+    const handleClickGood = () => {
+        setGood(good + 1)
+        setTotal(good + neutral + bad + 1)
+        setAvg(((good + 1) + (-1 * bad)) / (total + 1))
+        setPositive(((good + 1) / (total + 1)) * 100 + '%')
+    }
 
+    const handleClickNeutral = () => {
+        setNeutral(neutral + 1)
+        setTotal(good + neutral + bad + 1)
+        setPositive(((good) / (total + 1)) * 100 + '%')  
+        setAvg((good + (-1 * bad)) / (total + 1))
+    }
 
+    const handleClickBad = () => {
+        setBad(bad + 1)
+        setTotal(good + neutral + bad + 1)
+        setPositive(((good) / (total + 1)) * 100 + '%')
+        setAvg((good + (-1 * (bad + 1 ))) / (total + 1))
+    }
 
-    </div>
-  )
+    return (
+        <div>
+            <Header headerText="Anna palautetta"/>
+            <Button 
+                handleClick={handleClickGood}
+                text="Good"
+            />
+                <Button 
+                handleClick={handleClickNeutral}
+                text="Neutral"
+            />
+                <Button 
+                handleClick={handleClickBad}
+                text="Bad"
+            />
+            <Statistics
+                valueGood={good} 
+                valueNeutral={neutral}
+                valueBad={bad}
+                valueAvg={avg}
+                valuePositive={positive}
+                valueTotal={total}
+            />
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, 
-  document.getElementById('root')
+    document.getElementById('root')
 )
