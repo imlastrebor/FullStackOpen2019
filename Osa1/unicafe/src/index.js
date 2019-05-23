5166e635581872ce6f36c2916373c94ba90d0b3c
Robert Salmi <Robert.salmi@hotmail.com>
2019-05-23 12:07:46 +0300

    Osa 1.11
    Edited older code, removed state from total, avg and positive.

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -12,14 +12,16 @@ const Button = ({handleClick, text}) =>(
     <button onClick={handleClick}> 
         {text}
     </button>
-
 )
+
+
 
 const Statistic = ({text, value}) => {
     return(
-        <div>
-            <p>{text} {value}</p>
-        </div>
+            <tr>
+                <td> {text} </td>
+                <td> {value} </td>
+            </tr>
     )
 }
 
@@ -34,12 +36,16 @@ const Statistics = (props) => {
     return(
         <div>
             <Header headerText="statistiikka"/>
-            <Statistic text="hyvä" value={props.valueGood}/>
-            <Statistic text="neutraali" value={props.valueNeutral}/>
-            <Statistic text="huono" value={props.valueBad}/>
-            <Statistic text="yhteensä" value={props.valueTotal}/>
-            <Statistic text="keskiarvo" value={props.valueAvg}/>
-            <Statistic text="positiivista" value={props.valuePositive}/>
+            <table>
+                <tbody>
+                    <Statistic text="hyvä" value={props.valueGood}/>
+                    <Statistic text="neutraali" value={props.valueNeutral}/>
+                    <Statistic text="huono" value={props.valueBad}/>
+                    <Statistic text="yhteensä" value={props.valueTotal}/>
+                    <Statistic text="keskiarvo" value={props.valueAvg}/>
+                    <Statistic text="positiivista" value={props.valuePositive}/>
+                </tbody>
+            </table>
         </div>
     )
 
@@ -49,54 +55,59 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [total, setTotal] = useState(0)
-    const [avg, setAvg] = useState(0)
-    const [positive, setPositive] = useState(0)
+    // const [total, setTotal] = useState(0)
+    // const [avg, setAvg] = useState(0)
+    // const [positive, setPositive] = useState(0)
+
+    const total = good + bad + neutral
+    const avg = ((good) + (-1 * bad)) / (total)
+    const positive = ((good) / (total)) * 100 + '%'
 
     const handleClickGood = () => {
         setGood(good + 1)
-        setTotal(good + neutral + bad + 1)
-        setAvg(((good + 1) + (-1 * bad)) / (total + 1))
-        setPositive(((good + 1) / (total + 1)) * 100 + '%')
+        // setTotal(good + neutral + bad + 1)
+        // setAvg(((good + 1) + (-1 * bad)) / (total + 1))
+        // setPositive(((good + 1) / (total + 1)) * 100 + '%')
     }
 
     const handleClickNeutral = () => {
         setNeutral(neutral + 1)
-        setTotal(good + neutral + bad + 1)
-        setPositive(((good) / (total + 1)) * 100 + '%')  
-        setAvg((good + (-1 * bad)) / (total + 1))
+        // setTotal(good + neutral + bad + 1)
+        // setPositive(((good) / (total + 1)) * 100 + '%')  
+        // setAvg((good + (-1 * bad)) / (total + 1))
     }
 
     const handleClickBad = () => {
         setBad(bad + 1)
-        setTotal(good + neutral + bad + 1)
-        setPositive(((good) / (total + 1)) * 100 + '%')
-        setAvg((good + (-1 * (bad + 1 ))) / (total + 1))
+        // setTotal(good + neutral + bad + 1)
+        // setPositive(((good) / (total + 1)) * 100 + '%')
+        // setAvg((good + (-1 * (bad + 1 ))) / (total + 1))
     }
 
     return (
         <div>
-            <Header headerText="Anna palautetta"/>
-            <Button 
-                handleClick={handleClickGood}
-                text="Good"
-            />
-                <Button 
-                handleClick={handleClickNeutral}
-                text="Neutral"
-            />
-                <Button 
-                handleClick={handleClickBad}
-                text="Bad"
-            />
-            <Statistics
-                valueGood={good} 
-                valueNeutral={neutral}
-                valueBad={bad}
-                valueAvg={avg}
-                valuePositive={positive}
-                valueTotal={total}
-            />
+
+                    <Header headerText="Anna palautetta"/>
+                    <Button 
+                        handleClick={handleClickGood}
+                        text="Good"
+                    />
+                    <Button 
+                        handleClick={handleClickNeutral}
+                        text="Neutral"
+                    />
+                    <Button 
+                        handleClick={handleClickBad}
+                        text="Bad"
+                    />
+                <Statistics
+                    valueGood={good} 
+                    valueNeutral={neutral}
+                    valueBad={bad}
+                    valueAvg={avg}
+                    valuePositive={positive}
+                    valueTotal={total}
+                />
         </div>
     )
 }
