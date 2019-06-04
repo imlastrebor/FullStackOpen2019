4cbe37212b884d8db996043e189e482a79a813b7
Robert Salmi <Robert.salmi@hotmail.com>
2019-06-04 12:39:21 +0300

    puhelinluettelo 2.8 valmiina

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -5,14 +5,18 @@ const App = () => {
 
   const [newName, setNewName] = useState("lisää nimi");
 
-  const addName = e => {
+  const [newNumber, setNewNumber] = useState("lisää numero");
+
+  const addInfo = e => {
     e.preventDefault();
     const listObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     };
     setPersons(persons.concat(listObject));
     setNewName("");
+    setNewNumber("");
     if (persons.find(duplicate => duplicate.name === newName)) {
       alert(`${newName} on jo listalla`);
     }
@@ -22,31 +26,43 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handleNumberChange = e => {
+    setNewNumber(e.target.value);
+  };
+
   let uniq = Array.from(new Set(persons.map(uniqArr => uniqArr.name))).map(
     name => {
       return persons.find(uniqArr => uniqArr.name === name);
     }
   );
 
-  const personName = uniq.map(person => {
-    return <p key={person.id}>{person.name}</p>;
+  const personInfo = uniq.map(person => {
+    return (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    );
   });
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
       <form>
+        <h2>Lisää uusi</h2>
         <div>
           nimi: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={addName}>
+          numero: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addInfo}>
             lisää
           </button>
         </div>
       </form>
       <h2>Numerot</h2>
-      <> {personName} </>
+      <> {personInfo} </>
     </div>
   );
 };
