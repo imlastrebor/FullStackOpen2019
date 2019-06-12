d17f1d20ef83f806822ee9d9550d291b0bd80829
Robert Salmi <Robert.salmi@hotmail.com>
2019-06-12 20:40:00 +0300

    kohdat 2.9 ja 2.11 valmiina
    Palasin tekemään kohdan 2.9

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Button = ({ text, mission }) => {
   return (
@@ -8,35 +9,111 @@ const Button = ({ text, mission }) => {
   );
 };
 
-const Input = ({ content, handle }) => {
+const Input = ({ field, content, handle }) => {
   return (
     <div>
-      <input value={content} onChange={handle} />
+      {field} <input value={content} onChange={handle} />
     </div>
   );
 };
+
+// const FilteredNames = props => {
+//   const result = props.nameSearch(props.uniqueNames, props.filter);
+//   return <li>{result}</li>;
+// };
+
+const Filter = props => {
+  return (
+    <>
+      <form>
+        <Input
+          field={props.fieldFilter}
+          content={props.contentFilter}
+          handle={props.handleFilter}
+        />
+      </form>
+      {/* <ul>
+        <FilteredNames
+          nameSearch={props.nameSearch}
+          uniqueNames={props.uniqueNames}
+          filter={props.filter}
+        />
+      </ul> */}
+    </>
+  );
+};
+
 const PersonForm = props => {
   return (
     <>
       <form>
-        <Input content={props.contentName} handle={props.handleName} />
-        <Input content={props.contentNumber} handle={props.handleNumber} />
+        <Input
+          field={props.fieldName}
+          content={props.contentName}
+          handle={props.handleName}
+        />
+        <Input
+          field={props.fieldNumber}
+          content={props.contentNumber}
+          handle={props.handleNumber}
+        />
         <Button text={props.text} mission={props.mission} />
       </form>
     </>
   );
 };
 
-const Persons = ({ personInfo }) => {
-  return <ul>{personInfo}</ul>;
+const PersonInfo = props => {
+  if (props.filter.length > 0) {
+    // const result = props.nameSearch(props.uniqueNames, props.filter);
+    const result = props.uniqueNames.filter(o =>
+      o.name.toLocaleLowerCase().includes(props.filter.toLocaleLowerCase())
+    );
+    return result.map(person => {
+      return (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      );
+    });
+  } else {
+    return props.unique.map(person => {
+      return (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      );
+    });
+  }
+};
+
+const Persons = props => {
+  return (
+    <ul>
+      <PersonInfo
+        unique={props.unique}
+        // nameSearch={props.nameSearch}
+        uniqueNames={props.uniqueNames}
+        filter={props.filter}
+      />
+    </ul>
+  );
 };
 
 const App = () => {
   const [persons, setPersons] = useState([]);
 
+  const [filter, setFilter] = useState("");
+
   const [newName, setNewName] = useState("lisää nimi");
 
   const [newNumber, setNewNumber] = useState("lisää numero");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addInfo = e => {
     e.preventDefault();
@@ -46,11 +123,16 @@ const App = () => {
       id: persons.length + 1
     };
     setPersons(persons.concat(listObject));
+    setFilter("");
     setNewName("");
     setNewNumber("");
     if (persons.find(duplicate => duplicate.name === newName)) {
       alert(`${newName} on jo listalla`);
     }
+  };
+
+  const handleFilter = e => {
+    setFilter(e.target.value);
   };
 
   const handleNameChange = e => {
@@ -61,29 +143,51 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
-  let uniq = Array.from(new Set(persons.map(uniqArr => uniqArr.name))).map(
+  const unique = Array.from(new Set(persons.map(uniqArr => uniqArr.name))).map(
     name => {
       return persons.find(uniqArr => uniqArr.name === name);
     }
   );
 
-  const personInfo = uniq.map(person => {
-    return (
-      <li key={person.id}>
-        {person.name} {person.number}
-      </li>
-    );
+  const uniqueNames = unique.map(person => {
+    return person;
   });
+
+  // const nameSearch = (arr, string) => {
+  //   const search = arr.filter(o =>
+  //     o.toLocaleLowerCase().includes(string.toLocaleLowerCase())
+  //   );
+  //   return search;
+  // };
+  // console.log(nameSearch(uniqueNames, filter));
+
+  // const result = uniqueNames.filter(o =>
+  //   o.name.toLocaleLowerCase().includes(newName.toLocaleLowerCase())
+  // );
+  // console.log(
+  //   result.map(nimi => {
+  //     return nimi.name;
+  //   })
+  // );
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
 
+      <Filter
+        text="hae nimellä"
+        fieldFilter="haku"
+        contentFilter={filter}
+        handleFilter={handleFilter}
+      />
+
       <h2>Lisää uusi</h2>
 
       <PersonForm
+        fieldName="nimi"
         contentName={newName}
         handleName={handleNameChange}
+        fieldNumber="numero"
         contentNumber={newNumber}
         handleNumber={handleNumberChange}
         text="lisää"
@@ -91,9 +195,13 @@ const App = () => {
       />
 
       <h2>Numerot</h2>
-      <Persons personInfo={personInfo} />
+      <Persons
+        unique={unique}
+        // nameSearch={nameSearch}
+        uniqueNames={uniqueNames}
+        filter={filter}
+      />
     </div>
   );
 };
-
 export default App;
